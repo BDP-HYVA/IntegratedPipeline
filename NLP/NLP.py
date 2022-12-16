1198e53251ef5fa45bfe5b6551863bb9f1f179da
ChoHyoungSeo <francesco.johs@gmail.com>
2022-12-16 12:26:40 +0900

    init sync

---
+++ NLP/NLP.py
@@ -0,0 +1,31 @@
+import csv
+import chardet
+from pykospacing import Spacing
+from konlpy.tag import Okt
+
+okt = Okt()
+spacing = Spacing(rules=[''])
+spacing.set_rules_by_csv('./input_data.csv')
+
+stop_words = ["홍대", "연남동", "서울특별시", "마포구", "신촌", "건대", "명동", "명지대", "강남"] #인근 지역 명 추가
+
+a = open("./stopwords-ko.txt", 'r')
+aread = csv.reader(a)
+for word in aread:
+    stop_words.append(word[0])
+
+f = open("./input_data.csv", 'r')
+rdr = csv.reader(f)
+with open('output_data.csv', 'w', encoding='utf-8') as p:
+    writer=csv.writer(p)
+    for line in rdr:
+        ans = []
+        print("line", line)
+        temp_line = okt.nouns(spacing(line[0]))
+        for word in temp_line:
+            if len(word) != 1 and word not in stop_words:
+                ans.append(word)
+        print("ans", ans)
+        writer.writerow([ans])
+    p.close()
+f.close()
---
+++ apache-spark-zeppelin/zeppelin.py
@@ -0,0 +1,14 @@
+# zeppelin notebook
+%spark2.pyspark
+
+from pyspark.sql.functions import *
+from pyspark.sql.functions import date_format
+
+textfile = sc.textFile("hdfs:///user/maria_dev/final/preprocessed_data.csv")
+ans = textfile.flatMap(lambda line: line.split(","))\
+  .map(lambda line: (line.split(" ")[0],1))\
+  .reduceByKey(lambda a, b: a+b)\
+  .sortBy(lambda x: x[1], ascending=False)
+
+final_ans = ans.toDF()
+z.show(final_ans)
